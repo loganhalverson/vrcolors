ccba2a9e09c64154b2b172d0995aa4f42bcae127
Logan <halversonL@cwu.edu>
2023-09-27 12:37:02 -0700

    Added additional color functions.

--- src/Color.js
+++ src/Color.js
@@ -30,6 +30,42 @@ export const blendHexColors = (c0, c1, p) => {
 	);
 };
 
+// Function retrieved from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+const hexToRgb = (hex) => {
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
+
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result
+		? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16),
+		  }
+		: null;
+};
+
+// Function retrieved from https://www.30secondsofcode.org/js/s/rgb-to-hsl/
+const RGBToHSL = ({ r, g, b }) => {
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	const l = Math.max(r, g, b);
+	const s = l - Math.min(r, g, b);
+	const h = s ? (l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s) : 0;
+	return [60 * h < 0 ? 60 * h + 360 : 60 * h, 100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0), (100 * (2 * l - s)) / 2];
+};
+
+// Given a HEX color, returns it's equivalent HSL light value.
+export const getLightness = (color) => {
+	return RGBToHSL(hexToRgb(color))[2];
+};
+
+// Given a key (theme.background, theme.highlight, etc.), generates Tailwind shades for that color
+// and appends them to the given state object.
 export const generateShades = (key, setTheme) => {
 	setTheme((prevTheme) => {
 		return {
