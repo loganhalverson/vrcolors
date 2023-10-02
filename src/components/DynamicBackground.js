f77e798d8554eb4f390b1fc1a1b21e2873c12e1c
Logan <halversonL@cwu.edu>
2023-10-02 13:51:45 -0700

    Adjust DynamicBackground.js for luminosity.

--- src/components/DynamicBackground.js
+++ src/components/DynamicBackground.js
@@ -7,13 +7,25 @@ export const DynamicBackground = (props) => {
 	const [style, setStyle] = useState({ background: theme.background });
 
 	useEffect(() => {
-		setStyle({
-			...style,
-			background: `linear-gradient(to right top, ${shadeHexColor(theme.background, -0.8)}, 60%, ${shadeHexColor(theme.background, -0.4)}, 85%, ${shadeHexColor(
-				theme.background,
-				-0.8
-			)})`,
-		});
+		if (getLightness(theme.background) > 50) {
+			// Light background pattern
+			setStyle({
+				...style,
+				background: `linear-gradient(to right top, ${shadeHexColor(theme.background, -0.4)}, 60%, ${shadeHexColor(
+					theme.background,
+					-0.05
+				)}, 85%, ${shadeHexColor(theme.background, -0.4)})`,
+			});
+		} else {
+			// Dark background pattern
+			setStyle({
+				...style,
+				background: `linear-gradient(to right top, ${shadeHexColor(theme.background, -0.8)}, 60%, ${shadeHexColor(
+					theme.background,
+					-0.4
+				)}, 85%, ${shadeHexColor(theme.background, -0.8)})`,
+			});
+		}
 	}, [theme.background]);
 
 	return (
